179435f927eedcab86abe7d289014969db007e9f
Jim Yuan <jim@home.com>
2013-06-09 01:20:44 +0800

    初步生成网站所需页面框架

---
+++ build/index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+var hogan = require('hogan.js')
+	, fs    = require('fs')
+
+
+var layout, pages
+
+// compile layout template
+layout = fs.readFileSync(__dirname + '/../templates/layout.mustache', 'utf-8')
+layout = hogan.compile(layout)
+
+header = fs.readFileSync(__dirname + '/../templates/_header.mustache', 'utf-8')
+header = hogan.compile(header)
+
+footer = fs.readFileSync(__dirname + '/../templates/_footer.mustache', 'utf-8')
+footer = hogan.compile(footer)
+
+
+// iterate over pages
+fs.readdirSync(__dirname + '/../templates/pages').forEach(function (name) {
+
+	if (!name.match(/\.mustache$/)) return
+
+	var page = fs.readFileSync(__dirname  + '/../templates/pages/' + name, 'utf-8');
+	var context ={};
+	var pagename=name.replace(/\.mustache$/, '');
+
+	context["script"] =	pagename;
+	context["homeflag"] =  (pagename==="index")  ? "homebody" : "pagebody";
+
+	page = hogan.compile(page)
+	page = layout.render(context, {
+		body: page
+		, header: header
+		, footer: footer
+	})
+
+	fs.writeFileSync(__dirname + '/../docs/' + name.replace(/mustache$/, 'html'), page, 'utf-8')
+})
